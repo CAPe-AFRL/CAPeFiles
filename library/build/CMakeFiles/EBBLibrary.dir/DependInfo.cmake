fd2a956a095d26acafbbdf51d4f9e3845c7057bb
Donald Bartlett <donald@munchkin>
2016-01-21 15:48:30 -0500

    added library

---
+++ library/build/CMakeFiles/EBBLibrary.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/debian/exploringBB/library/build/CMakeFiles/CompilerIdCXX/CMakeCXXCompilerId.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/CMakeFiles/CompilerIdCXX/CMakeCXXCompilerId.cpp.o"
+  "/home/debian/exploringBB/library/bus/BusDevice.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/bus/BusDevice.cpp.o"
+  "/home/debian/exploringBB/library/bus/I2CDevice.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/bus/I2CDevice.cpp.o"
+  "/home/debian/exploringBB/library/bus/SPIDevice.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/bus/SPIDevice.cpp.o"
+  "/home/debian/exploringBB/library/display/LCDCharacterDisplay.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/display/LCDCharacterDisplay.cpp.o"
+  "/home/debian/exploringBB/library/display/SevenSegmentDisplay.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/display/SevenSegmentDisplay.cpp.o"
+  "/home/debian/exploringBB/library/docs/front_page.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/docs/front_page.cpp.o"
+  "/home/debian/exploringBB/library/gpio/GPIO.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/gpio/GPIO.cpp.o"
+  "/home/debian/exploringBB/library/gpio/PWM.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/gpio/PWM.cpp.o"
+  "/home/debian/exploringBB/library/gpio/util.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/gpio/util.cpp.o"
+  "/home/debian/exploringBB/library/motor/DCMotor.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/motor/DCMotor.cpp.o"
+  "/home/debian/exploringBB/library/motor/Servo.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/motor/Servo.cpp.o"
+  "/home/debian/exploringBB/library/motor/StepperMotor.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/motor/StepperMotor.cpp.o"
+  "/home/debian/exploringBB/library/network/SocketClient.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/network/SocketClient.cpp.o"
+  "/home/debian/exploringBB/library/network/SocketServer.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/network/SocketServer.cpp.o"
+  "/home/debian/exploringBB/library/sensor/ADXL345.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/sensor/ADXL345.cpp.o"
+  "/home/debian/exploringBB/library/sensor/ITG3200.cpp" "/home/debian/exploringBB/library/build/CMakeFiles/EBBLibrary.dir/sensor/ITG3200.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../bus"
+  "../display"
+  "../gpio"
+  "../motor"
+  "../network"
+  "../sensor"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
